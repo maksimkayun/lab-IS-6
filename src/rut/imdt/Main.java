6747cf363a3e94a0c07da226d81bd794eb020413
makskayunPC <maks.kayn@yandex.com>
2021-04-25 12:17:45 +0300

    добавлена функция setTwoKey
    setTwoKey - записывает в ячейки таблиц по ключу 142-й ASCII символ

--- src/rut/imdt/Main.java
+++ src/rut/imdt/Main.java
@@ -10,7 +10,50 @@ public class Main {
     private static Scanner sc = new Scanner(System.in);
     private static List<Integer> key = new ArrayList<>();
     private static List<Integer> keyTwo = new ArrayList<>();
+    private static List<String[][]> tableList = new ArrayList<>();
+
+    public static void createTables(String message, int n, int m) {
+        int length = message.length();
+        int quantity = length / (n * m);
+        if (length % (n * m) != 0) {
+            quantity++;
+        }
+        quantity++;
+        for (int i = 0; i < quantity; i++) {
+            tableList.add(new String[n][m]);
+        }
+    }
+
+    public static void setKeyTwo() {
+        char symbol = 142;
+        String ch = String.valueOf(symbol);
+
+        for (int i = 0; i < tableList.size(); i++) {
+            for (int j = 0; j < keyTwo.size(); j += 2) {
+                tableList.get(i)[keyTwo.get(j)][keyTwo.get(j+1)] = ch;
+            }
+        }
+    }
+
+    public static String enc(String message, int n, int m) {
+        String output = "";
+        createTables(message, m, n);
+        setKeyTwo();
+
+        return output;
+    }
+
+    public static String dec(String message, int n, int m) {
+        String output = "";
+        createTables(message, m, n);
+        setKeyTwo();
+
+        return output;
+    }
+
     public static void main(String[] args) {
+        //============//============//
+        //           INPUT          //
         System.out.print("Введите кол-во столбцов таблиц: ");
         int m = Integer.parseInt(sc.nextLine());
 
@@ -21,11 +64,23 @@ public class Main {
         String values = sc.nextLine();
         key = Arrays.stream(values.split(" ")).map(Integer::parseInt).collect(Collectors.toList());
 
-        System.out.printf("Введите координаты пустых ячеек (формат x,y x,y...) столбцы x < %d, строки y < %d): ", m, n);
+        System.out.printf("Введите координаты пустых ячеек (формат x,y x,y...) строки x < %d, " +
+                "столбцы y < %d, не более %d пар: ", n - 1, m - 1, n * m);
         String[] inKeyTwo = sc.nextLine().split(" ");
         for (int i = 0; i < inKeyTwo.length; i++) {
             keyTwo.add(Integer.parseInt(inKeyTwo[i].split(",")[0]));
             keyTwo.add(Integer.parseInt(inKeyTwo[i].split(",")[1]));
         }
+        System.out.print("Введите сообщение для шифрования: ");
+        String message = sc.nextLine();
+        //          END INPUT       //
+        //============//============//
+
+        message = enc(message, n, m);
+        System.out.printf("Зашифрованное сообщение: %s", message);
+        tableList.clear();
+
+        message = dec(message, n, m);
+        System.out.printf("\nРасшифрованное сообщение: %s", message);
     }
 }
